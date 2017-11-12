571f5ba28103ded3fa98d554923bdf8af52ab135
WYonghui <810981846@11.com>
2017-11-12 09:01:04 +0800

    2017.11.12转为maven项目

---
+++ src/main/java/nioTest/CommonParameter.java
@@ -0,0 +1,12 @@
+package nioTest;
+
+/**
+ * Created by 10564 on 2017-11-09.
+ */
+public class CommonParameter {
+    public static String srcPath = "/home/wang/sda13/projects/text256mb";
+            //"/root/wang/projects/hiberfil3";
+    public static String dstPath = "/home/wang/sda13/projects/text";
+                    //"/root/wang/projects/hiberfil8";
+    public static int count = 1024*1024;   //每次传输1MB
+}
